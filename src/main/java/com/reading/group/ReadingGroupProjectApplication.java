2c3fbacc6296ee806ff7d4528581df809ef66474
hyunjun <hyunjun@DESKTOP-CGTR5BN>
2024-09-11 23:21:31 +0900

    메인 어플리케이션 코드 추가

---
+++ src/main/java/com/reading/group/ReadingGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.reading.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingGroupProjectApplication.class, args);
+	}
+
+}
